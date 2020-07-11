add8b72146d6124189d330522b73910bb7f71fb0
Yuki Sobue <yuki.sobue@gmail.com>
2020-07-11 20:42:25 +0900

    add default encoding

--- sjis_checker.rb
+++ sjis_checker.rb
@@ -1,13 +1,23 @@
 require "./checker_mode.rb"
 
+
 class SjisChecker
-  attr_accessor :mode
+  attr_accessor :mode, :encoding
 
   def initialize(mode = CheckerMode::Default)
-    @mode = mode
+    @encoding = "SJIS"
+    @mode = mode.new
+  end
+
+  def encode(str)
+    str.encode(encoding)
   end
 
   def scan(string)
     mode.execute(string)
+  end
+
+  def set_replacer(string)
+    mode.set_replacer(string)
   end
 end
